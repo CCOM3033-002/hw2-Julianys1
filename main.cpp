e6abfe37b8298d7fb5564857d424a4d4b7c39372
Julianys1 <137523206+Julianys1@users.noreply.github.com>
2025-03-11 12:19:46 -0400

    paste of final version
    problems with commit (LGA)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
+
+
+/*
+Julianys Tirado Alicea
+Julianys1
+801225449
+
+*/
+
+
 #include<iostream>
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
+
+  //Mensaje inicial 
+
+cout<< "Este programa suma dos fracciones.\n\n";
   
+
+// declarar variables
+
+int num1, den1, num2, den2, resultadoNum, resultadoDen;
+
+
+
+//entrada primera fraccion
+cout<< "Entre el numerador de la 1ra fracción: ";
+cin>>num1;
+
+cout<<"Entre el denominador de la 1ra fracción:";
+cin>>den1;
+
+//entrada segunda fraccion
+
+cout<< "Entre el numerador de la 2ra fracción: ";
+cin>>num1;
+
+cout<<"Entre el denominador de la 2ra fracción:";
+cin>>den1;
+//calculo de la suma de las fracciones 
+
+resultadoNum = (num1 * den2) + (num2 * den1); 
+resultadoDen = den1 * den2;
+
+
+//Mostrar el resultado 
+
+cout << "La suma es" <<resultadoNum << "/" <<resultadoDen;
   return 0;
 }
